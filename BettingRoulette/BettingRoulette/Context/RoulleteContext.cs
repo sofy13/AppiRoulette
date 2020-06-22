df29b5b04d1efac5023e491334f594a027ee1cee
SOFIA ALDANA <saldanao@uqvirtual.edu.co>
2020-06-21 23:41:25 -0500

    Se establece conexion con la base de datos y se estabece el contexto

---
+++ BettingRoulette/BettingRoulette/Context/RoulleteContext.cs
@@ -0,0 +1,52 @@
+﻿using BettingRoulette.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BettingRoulette.Context
+{
+    public partial class RouletteContext : DbContext
+    {
+
+        public RouletteContext()
+        {
+        }
+
+        public RouletteContext(DbContextOptions<RouletteContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Bet> Bet { get; set; }
+        public virtual DbSet<Roulette> Roulette { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Bet>(entity =>
+            {
+                entity.HasKey(e => e.IdBet)
+                    .HasName("bet_pkey");
+
+                entity.ToTable("bet");
+
+                entity.Property(e => e.IdRouletteBet).HasColumnName("idRouletteBet");
+
+                entity.Property(e => e.idUserBet).HasColumnName("idUserBet");
+            });
+
+            modelBuilder.Entity<Roulette>(entity =>
+            {
+                entity.HasKey(e => e.IdRoulette)
+                    .HasName("roulette_pkey");
+
+                entity.ToTable("roulette");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
