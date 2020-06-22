4afb14235050cb41cf9d9471ec790faa9b20baaa
SOFIA ALDANA <saldanao@uqvirtual.edu.co>
2020-06-21 23:33:56 -0500

    Se crea clase Bet, la cual representa una apuesta.

---
+++ BettingRoulette/BettingRoulette/Entities/Bet.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BettingRoulette.Entities
+{
+    public partial class Bet
+    {
+        public Bet()
+        {
+            NumberBet = -1;
+            StateBet = true;
+        }
+
+        [Key]
+        [Column(Order = 1, TypeName = "serial")]
+        public long IdBet { get; set; }
+        public long AmountBet { get; set; }
+        public long IdRouletteBet { get; set; }
+        public string idUserBet { get; set; }
+        public int NumberBet { get; set; }
+        public string ColorBet { get; set; }
+        public bool StateBet { get; set; }
+    }
+}
